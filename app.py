7ac57d9fce23f39e0443462676382c39dd5c7690
sherbw28 <sherbw28@outlook.jp>
2023-03-18 23:59:48 +0900

    formテスト

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/result', methods=['GET','POST'])
+def result():
+    if request.method == 'POST':
+        test = request.form.get('test')
+        return render_template('result.html', test = test)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
