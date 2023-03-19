294eb71a2b8549fbb04a8fa9691fec6b8c4ad15c
sherbw28 <sherbw28@outlook.jp>
2023-03-20 00:24:11 +0900

    テストページ作成

--- aws_process.py
+++ aws_process.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, Blueprint
+import os
 
 aws_process = Blueprint('aws_process', __name__)
 
-@aws_process.route("/test")
+@aws_process.route("/test", methods=['GET', 'POST'])
 def test():
-    return render_template('test.html')
+    if request.method == 'POST':
+        test = request.form.get('test')
+        file = request.files['img']
+        file.save(os.path.join('./static/img',file.filename))
+        return render_template('test.html', test = file.filename)
+    else:
+        return render_template('index.html')
